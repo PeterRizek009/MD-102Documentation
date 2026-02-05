85770d0752ff068c9a6b9286c9ed1a5981e39fca
Peter Rezik <petershahin00@gmail.com>
2026-02-05 13:06:39 +0400

    Refactor App component with updated routes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,47 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import OnPremiseDeployment from "./pages/OnPremiseDeployment";
+import WindowsAutopilotUltimateGuide from "./pages/WindowsAutopilotUltimateGuide";
+import IdentityIntegrationGuide from "./pages/IdentityIntegrationGuide";
+import IntuneGroupsDocumentation from "./pages/IntuneGroupsDocumentation";
+import RBACDocumentation from "./pages/RBACDocumentation";
+import ConditionalAccessUltimateGuide from "./pages/ConditionalAccessUltimateGuide";
+import IntuneDeviceEnrollmentDocumentation from "./pages/IntuneDeviceEnrollmentDocumentation";
+import ConfigurationProfilesDocumentation from "./pages/ConfigurationProfilesDocumentation";
+import CompliancePoliciesDocumentation from "./pages/CompliancePoliciesDocumentation";
+import EndpointSecurityGuide from "./pages/EndpointSecurityGuide";
+import MDEFullDocumentation from "./pages/MDEFullDocumentation";
+import Microsoft365AppsDocumentation from "./pages/Microsoft365AppsDocumentation";
+import IntuneAppsUltimatePlaybook from "./pages/IntuneAppsUltimatePlaybook";
+import WindowsUpdateForBusinessGuide from "./pages/WindowsUpdateForBusinessGuide";
+import IntuneReportsDocumentation from "./pages/IntuneReportsDocumentation";
+
 export default function App() {
-return (
-<Router>
-<Routes>
-<Route path="/" element={<Home />} />
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
 
+      <Route path="/on-premise-deployment" element={<OnPremiseDeployment />} />
+      <Route path="/windows-autopilot" element={<WindowsAutopilotUltimateGuide />} />
 
-{/* Deployment */}
-<Route path="/deployment/on-premise" element={<OnPremiseDeployment />} />
-<Route path="/deployment/autopilot" element={<Placeholder title="Autopilot" />} />
+      <Route path="/identity-integration" element={<IdentityIntegrationGuide />} />
+      <Route path="/groups" element={<IntuneGroupsDocumentation />} />
+      <Route path="/rbac" element={<RBACDocumentation />} />
+      <Route path="/conditional-access" element={<ConditionalAccessUltimateGuide />} />
 
+      <Route path="/device-enrollment" element={<IntuneDeviceEnrollmentDocumentation />} />
+      <Route path="/configuration-profiles" element={<ConfigurationProfilesDocumentation />} />
 
-{/* Devices */}
-<Route path="/devices/configuration" element={<ConfigurationProfile />} />
-<Route path="/devices/enrollment" element={<Placeholder title="Enrollment" />} />
-<Route path="/devices/autopilot" element={<Placeholder title="Autopilot" />} />
-<Route path="/devices/zero-touch" element={<Placeholder title="Zero Touch Deployment" />} />
+      <Route path="/compliance-policies" element={<CompliancePoliciesDocumentation />} />
+      <Route path="/endpoint-security" element={<EndpointSecurityGuide />} />
+      <Route path="/mde" element={<MDEFullDocumentation />} />
 
+      <Route path="/microsoft-365-apps" element={<Microsoft365AppsDocumentation />} />
+      <Route path="/intune-apps" element={<IntuneAppsUltimatePlaybook />} />
+      <Route path="/windows-update" element={<WindowsUpdateForBusinessGuide />} />
 
-{/* Identity */}
-<Route path="/identity/users" element={<Placeholder title="Users" />} />
-<Route path="/identity/groups" element={<Placeholder title="Groups" />} />
-<Route path="/identity/conditional-access" element={<Placeholder title="Conditional Access" />} />
-
-
-{/* Security */}
-<Route path="/security/compliance" element={<Placeholder title="Compliance Policies" />} />
-<Route path="/security/defender" element={<Placeholder title="Defender for Endpoint" />} />
-
-
-{/* Apps & Updates */}
-<Route path="/apps/deployment" element={<Placeholder title="App Deployment" />} />
-<Route path="/updates/windows" element={<Placeholder title="Windows Updates" />} />
-
-
-{/* Fallback */}
-<Route path="*" element={<Placeholder title="404 - Not Found" />} />
-</Routes>
-</Router>
-);
+      <Route path="/reports" element={<IntuneReportsDocumentation />} />
+    </Routes>
+  );
 }
